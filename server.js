070f8a30ff638812e2579019a3294f6f29d391f5
MaximilianMcC <max@maximilian.co.nz>
2023-06-29 07:06:19 +1000

    Posts can be added and read from the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const sqlite = require("sqlite3");
+const bodyParser = require("body-parser");
+const sqlite = require("sqlite3").verbose();
 const path = require("path");
 
 
@@ -7,10 +8,66 @@ const path = require("path");
 const port = 3000;
 const app = express();
 
-// Serve the static elements
+// Serve the static elements and allow body parsing
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+
+// Create the database
+const database = new sqlite.Database("database.sqlite");
+
+// Add the posts table if it doesn't already exist
+//! `content` is JSON code. Might not be the best practice but it works
+// TODO: don't store upvotes and downvotes severalty. Have a signed int and it can be negative or positive
+database.run(`CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, upvotes INTEGER, downvotes INTEGER)`);
 
 
+
+// Create and add a post to the database
+app.post("/add-post", (request, response) => {
+	console.log("New-post called");
+
+	// Get the data from the post body
+	const body = request.body;
+
+	// Extract the data from the body
+	const title = body["title"];
+	const content = body["content"];
+	const upvotes = body["upvotes"];
+	const downvotes = body["downvotes"];
+
+	// Add the data to the database
+	database.run(`INSERT INTO posts(title, content, upvotes, downvotes) VALUES (?, ?, ?, ?)`,
+		[title, content, upvotes, downvotes],
+		(error) => {
+			
+			// Check for errors
+			if (error) {
+				response.status(500).send("Error adding post.")
+				return;
+			}
+			
+			response.status(200).send("Post has been added.")
+		}
+	);
+});
+
+
+
+// TODO: don't get every single post every single time
+app.get("/posts", (request, response) => {
+	console.log("posts called");
+
+	database.all(`SELECT * FROM posts`, (error, data) => {
+		// Check for if there was an error
+		if (error) {
+			response.status(500).send("Error while getting posts.");
+			return;
+		}
+
+		// Return all of the notes
+		response.status(200).json(data);
+	});
+});
 
 
 
