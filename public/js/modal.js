f7db5cdd50c3e9eab548e91f8b60ca52b83e36dc
Max <max@maximilian.co.nz>
2023-08-17 22:24:04 +1200

    Open modal buttons now work

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,3 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+	// Find all of the close buttons inside modals
+	const modalCloseButtons = document.querySelectorAll("#modalCloseButton");
+	modalCloseButtons.forEach((button) => {
+
+		// Close the modal when they click on it
+		button.addEventListener("click", () => {
+
+			const modal = button.closest(".modal-wrapper");
+			toggleModal(modal.id);
+		});
+	});
+
+	
+	// Find all of the open buttons
+	const modalOpenButtons = document.querySelectorAll("button[modalId]");
+	modalOpenButtons.forEach(button => {
+		
+		// Open the modal when they click on it
+		button.addEventListener("click", () => {
+			
+			toggleModal(button.getAttribute("modalId"));
+		});
+	});
+
+});
+
+
+
+
 function toggleModal(modalId) {
 
 	const modalElement = document.getElementById(modalId);
