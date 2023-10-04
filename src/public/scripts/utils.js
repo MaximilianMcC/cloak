92d14b8b96f8509b149831c9326875875efe9a2c
Max <max@maximilian.co.nz>
2023-10-05 01:33:27 +1300

    Added HTTP GET and POST methods

--- src/public/scripts/utils.js
+++ src/public/scripts/utils.js
@@ -1,1 +1,18 @@
-console.log("h");
+// HTTP post request
+async function post(url, data) {
+
+	const response = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(data)
+	});
+}
+
+// HTTP get request
+async function get(url) {
+
+	const response = await fetch(url);
+	return response.json()
+}
