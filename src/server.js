313bd0ff5668d7478e580598ef68c827a8dca83e
Max <max@maximilian.co.nz>
2023-10-08 03:47:30 +1300

    Posts can be gotten from database

--- src/server.js
+++ src/server.js
@@ -18,22 +18,77 @@ const mongoUri = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(mongoUri);
 
 // Create a post
-// TODO: Add try/catch
 app.post("/create-post", async (request, response) => {
 	console.log("/create-post");
 
-	// Connect to the database
-	await mongoClient.connect();
+	try {
+		// Connect to the database
+		await mongoClient.connect();
 
-	// Get the cloak database, and posts collection
-	const database = mongoClient.db(mongoName);
-	const posts = database.collection("posts");
+		// Get the cloak database, and posts collection
+		const database = mongoClient.db(mongoName);
+		const posts = database.collection("posts");
 
-	// Add the posts request body to the database
-	await posts.insertOne(request.body);
+		// Add the posts request body to the database
+		await posts.insertOne(request.body);
 
-	// Done
-	response.send(200);
+		// Done
+		response.send(200);
+		return;
+
+	} catch (error) {
+
+		// return 500 to user
+		console.error(`Error while creating post:\n${error}`);
+		response.send(500);
+		return;
+	}
+});
+
+
+
+// Get x most recent posts
+app.get("/posts", async (request, response) => {
+	console.log("GET /posts");
+
+	// Save all of the data
+	let data;
+
+	// Settings
+	let postsToGet = request.query.count;
+	if (postsToGet == 0) postsToGet = 1;
+
+	try {
+		// Connect to the database
+		await mongoClient.connect();
+
+		// Get the cloak database, and posts collection
+		const database = mongoClient.db(mongoName);
+		const posts = database.collection("posts");
+
+		// Get the timestamp of posts to get from
+		const timestamp = Date.now();
+		console.log(timestamp);
+
+		// Query the database for all posts where the date is <= the current time
+		//? $-1 is descending order
+		const query = { date: { $lte: timestamp } };
+		const result = posts.find(query).sort({ date: -1 }).limit(parseInt(postsToGet));
+
+		// Get all of the data
+		const data = await result.toArray();
+
+		// Send the data back to the client
+		response.send(data, 200);
+		return;
+
+	} catch (error) {
+		
+		// return 500 to user
+		console.error(`Error while getting posts:\n${error}`);
+		response.send(500);
+		return;
+	}
 });
 
 
